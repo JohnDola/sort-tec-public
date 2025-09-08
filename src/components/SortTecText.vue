ba6a8628f3e5b58e8508214842fc90d8696057f0
JohnDola <j.m.dola1990@gmail.com>
2025-09-08 09:03:12 +0000

    Deploying to main from @ JohnDola/sort-tec-code@239936a5d0181427db7d11194c890a946e609bf7 🚀

--- src/components/SortTecText.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <span class="font-montserrat">
-        <span class="font-medium">Sort</span><span class="font-bold text-[var(--color-primary)]">Tec</span>
-    </span>
-</template>
-
-<script setup lang="ts">
-// Simple text component for consistent SortTec branding
-</script>
